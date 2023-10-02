b787b61c286b4c084a87b641c7422c46bdb78e4f
Frooddie <freddie.andreasson@student.nbi-handelsakademin.se>
2023-10-02 10:50:17 +0200

    Final code for project

--- Inlämning1/Inlämning1/Program.cs
+++ Inlämning1/Inlämning1/Program.cs
@@ -5,14 +5,28 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to a guessing game, I'm thinking of a number between 1-100, which number am I thinking of?");
-            int answer = 48;
-            int guess = int.Parse(Console.ReadLine());
-            bool GameOver = false;
-            List<int> guesslist = new List<int>();
+            bool GameOver = false; //For not making game repeat itself indefinetly
+            List<int> guesslist = new List<int>(); //To create a list of all the guesses you've made
             while (GameOver == false)
             {
+                //Creation of which random number the program is thinking of
+                Random random = new Random();
+                int answer = random.Next(1, 101);
+                int guess = int.MaxValue;
+                while (true)
+                {
+                    try //In case that user enters a string
+                    {
+                        guess = int.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch 
+                    {
+                        Console.WriteLine("Please enter a number as stated prior!");
+                    }
+                }
                 guesslist.Add(guess);
-                if (guess < answer)
+                if (guess < answer) //If your guess is lower than the answer
                 {
                     Console.WriteLine("Your guess was: " + guess + " and that was lower than what I'm thinking of!");
                     Console.WriteLine("Would you like to: a. Guess again, b. Give up? or c. See all your previous guesses?");
@@ -23,20 +37,20 @@
                         guess = int.Parse(Console.ReadLine());
                         continue;
                     }
-                    else if (choice == "c")
+                    else if (choice == "b")
                     {
                         Console.WriteLine(string.Join(", ", guesslist));
                         Console.WriteLine("Going back to the latest guess!");
                         continue;
                     }
-                    else 
+                    else //Option "c"
                     {
                         Console.WriteLine("Better luck next time!");
                         GameOver = true;
                         break;
                     }
                 }
-                else if (guess > answer)
+                else if (guess > answer) //If your guess is higher than the answer
                 {
                     Console.WriteLine("Your guess was: " + guess + " and that was higher than what I'm thinking about!");
                     Console.WriteLine("Would you like to: a. Guess again, b. Give up? or c. See all your previous guesses?");
@@ -47,20 +61,20 @@
                         guess = int.Parse(Console.ReadLine());
                         continue;
                     }
-                    else if (choice == "c")
+                    else if (choice == "b")
                     {
                         Console.WriteLine(string.Join(", ", guesslist));
                         Console.WriteLine("Going back to the latest guess!");
                         continue;
                     }
-                    else 
+                    else //Option "c"
                     {
                         Console.WriteLine("Better luck next time!");
                         GameOver = true;
                         break;
                     }
                 }
-                else
+                else //If your guess equals answer
                 {
                     Console.WriteLine("Congratulations! You guessed " + guess + " and that is correct!");
                     Console.WriteLine("Game shutting down!");
